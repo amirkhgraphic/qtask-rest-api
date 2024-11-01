5e0f9cb9d3c16c285a783b3af9561dc8a5e7ea3f
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-11-01 14:20:42 +0330

    docs(*): add OpenAPI 3 documentation setup

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
 ]
 
 MIDDLEWARE = [
@@ -88,14 +90,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
+    # JWT Authentication configuration
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
+    # Invalid data label
     'NON_FIELD_ERRORS_KEY': 'invalid data',
 
+    # Pagination Configuration
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.CustomCursorPagination',
     'PAGE_SIZE': 20,
+
+    # OpenAPI 3 documentation
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
@@ -106,4 +114,34 @@ SIMPLE_JWT = {
     'ALGORITHM': 'RS256',
     'SIGNING_KEY': Path('keys/private.pem').read_text(),
     'VERIFYING_KEY': Path('keys/public.pem').read_text(),
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Key-Value REST API',
+    'DESCRIPTION': (
+        "This Key-Value REST API offers secure and efficient data management, "
+        "featuring a flexible key-value storage system that supports both simple "
+        "and nested pairs. Built with Django and Django REST Framework, the API "
+        "is secured using JWT authentication with RS256 encryption, ensuring "
+        "privacy and data integrity. Key features include:\n\n"
+        "- **User Authentication**: Secure JWT-based user authentication for "
+        "access control.\n"
+        "- **Key-Value Storage**: Flexible handling of key-value data, supporting "
+        "both flat and nested structures.\n"
+        "- **Cursor Pagination**: Efficiently handles large data sets, improving "
+        "client performance.\n"
+        "- **Database Optimization**: PostgreSQL-backed with advanced indexing "
+        "for optimized querying.\n"
+        "- **Comprehensive Documentation**: OpenAPI 3 schema for easy integration.\n"
+        "- **Testing and Coverage**: Ensures reliability and maintainability with "
+        "automated tests and coverage reports.\n\n"
+        "Built for production, this API is well-suited for high-traffic applications "
+        "requiring secure, scalable, and reliable data storage."
+    ),
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
 }
