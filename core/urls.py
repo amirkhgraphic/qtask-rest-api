b3425d041873cfea5e8c28386c0d1bc61b917a21
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-11-01 14:21:20 +0330

    docs(*): document API endpoints and schema (Swagger-UI & Redoc)

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,18 @@ from django.urls import (
     include,
 )
 
+from drf_spectacular import views
+
 urlpatterns = [
+    # django admin panel
     path('admin/', admin.site.urls),
+
+    # apps endpoints
     path('users/', include(('users.urls', 'users'), namespace='users')),
     path('pairs/', include(('pairs.urls', 'pairs'), namespace='pairs')),
+
+    # documentation endpoints
+    path('api/schema/', views.SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger-ui/', views.SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', views.SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
