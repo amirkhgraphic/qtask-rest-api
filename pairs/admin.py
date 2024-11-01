9770fd568220b643022d3a9d7d4ef9312f6287ef
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-11-01 11:43:40 +0330

    chore(pairs): configure admin panel for pairs management

--- pairs/admin.py
+++ pairs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import KeyValue
+
+
+class KeyValueInline(admin.TabularInline):
+    model = KeyValue
+    extra = 1
+    fields = ('key', 'type', 'value')
+
+
+@admin.register(KeyValue)
+class KeyValueAdmin(admin.ModelAdmin):
+    list_display = ('key', 'type', 'parent', 'value')
+    search_fields = ('key',)
+    inlines = [KeyValueInline]
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            kwargs["queryset"] = KeyValue.objects.filter(type=KeyValue.KeyValueTypeChoices.NESTED)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
