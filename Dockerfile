eeb186f88071767229f1ac528d13a9b0af4fe182
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-11-02 18:43:55 +0330

    fix(docker): run django application and db with docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,24 @@
-# Use an official Python runtime as a parent image
-FROM python:3.11.5
+# Dockerfile
 
-# Set the working directory
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Set working directory
 WORKDIR /app
 
-# Copy the requirements file
-COPY requirements.txt .
-
 # Install dependencies
+COPY requirements.txt /app/
 RUN pip install -r requirements.txt
 
-# Copy the current directory contents into the container at /app
-COPY . .
+# Copy project files to the working directory
+COPY . /app/
 
-# Collect static files
-RUN python manage.py collectstatic --noinput
-
-# Expose the port for Gunicorn
+# Expose the port the app runs on
 EXPOSE 8000
 
-# Run Gunicorn server
-CMD ["gunicorn", "core.wsgi:application", "--bind", "0.0.0.0:8000"]
+# Command to run the application
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "core.wsgi:application"]
--- core/settings/production.py
+++ core/settings/production.py
@@ -1,19 +1,28 @@
 from .base import *
 from core.settings import env
 
+import os
+import dj_database_url
+
 DEBUG = False
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
-DATABASES = {
-    "default": {
-        "ENGINE": env("DB_ENGINE"),
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+database_url = os.getenv("DATABASE_URL", default=None)
+if database_url:
+    DATABASES = {
+        'default': dj_database_url.config(default=database_url)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": env("DB_ENGINE"),
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USER"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+        }
+    }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
